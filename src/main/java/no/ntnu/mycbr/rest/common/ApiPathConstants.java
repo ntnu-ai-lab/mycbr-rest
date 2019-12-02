a3876506debad8b7c64ccb003535bca1538735e8
amar <amar.jaiswal@ntnu.no>
2019-12-02 21:39:37 +0100

    critical: updated the path constants

--- src/main/java/no/ntnu/mycbr/rest/common/ApiPathConstants.java
+++ src/main/java/no/ntnu/mycbr/rest/common/ApiPathConstants.java
@@ -1,98 +1,159 @@
 package no.ntnu.mycbr.rest.common;
 
+/**
+ * This class defines constants for RequestMapping path, RequestMapping headers, PathVariable, RequestParam, and default values.
+ *  <br>
+ * <br> <b>Abbreviations Used: </b>
+ * <ul>
+ * <li> ID: Identifier
+ * <li> ATTR: Attribute
+ * <li> SIM : Similarity
+ * <li> AMAL: Amalgamation
+ * </ul>
+ * @author Amar Jaiswal
+ *
+ */
 public final class ApiPathConstants {
 
-
-    public static final String CONCEPT_STR = "concept";
-    public static final String CONCEPT_ID_STR = "conceptID";
+    // The media type constants
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String ACCEPT_APPLICATION_JSON = "Accept=application/json";
     
-    public static final String CASEBASE_STR = "casebase";
-    public static final String CASEBASE_ID_STR = "casebaseID";
-    
-    public static final String CASE_ID_STR = "caseID";
-    public static final String CASE_IDS_STR = "caseIDs";
-    
-    public static final String INSTANCE_ID_STR = "instanceID";
-    
-    public static final String AMALGAMATION_FUNCTION_ID_STR = "amalgamationFunctionID";
-    
-    public static final String AMALGAMATION_FUNCTION_STR = "amalgamation function";
-
-    public static final String DEFAULT_NO_OF_CASES = "-1";
-    public static final String NO_OF_RETURNED_CASES = "k";
-
     // Need to match below variables according to the cbr project
     public static final String DEFAULT_CASEBASE = "CaseBase0"; 
     public static final String DEFAULT_CONCEPT = "Car"; 
-    public static final String DEFAULT_AMALGAMATION_FUNCTION = "CarFunc"; 
+    public static final String DEFAULT_AMAL_FUNCTION = "CarFunc"; 
+    public static final String DEFAULT_ATTR_ID = "Color"; 
+    
+    // The number of retrieved cases per query as preferred by the user.
+    public static final String DEFAULT_NO_OF_CASES = "-1";
+    public static final String NO_OF_RETURNED_CASES = "k";
 
-    public static final String APPLICATION_JSON = "application/json";
-    public static final String ACCEPT_APPLICATION_JSON = "Accept=application/json";
+    
+    // myCBR-rest API: core vocabulary - single
+    public static final String 
+    CONCEPT 		= "concept",
+    CASEBASE 		= "casebase",
+    AMAL_FUNCTION 	= "amalgamationFunction",
+    ATTR 		= "attribute",
+    CASE 		= "case",
+    INSTANCE		= "instance",
+    VALUE 		= "value",
+    SIM_FUNCTION   	= "similarityFunction";
+    
+    // myCBR-rest API: core vocabulary - multiple
+    public static final String 
+    S 			= "s",
+    CONCEPTS 		= CONCEPT 	+ S,
+    CASEBASES 		= CASEBASE 	+ S,
+    AMAL_FUNCTIONS	= AMAL_FUNCTION + S,
+    ATTRS 		= ATTR 		+ S,
+    CASES 		= CASE 		+ S,
+    INSTANCES		= INSTANCE 	+ S,
+    VALUES 		= VALUE 	+ S,
+    SIM_FUNCTIONS 	= SIM_FUNCTION	+ S;
+    
+    
+    // myCBR-rest API: ID vocabulary
+    public static final String 
+    ID 			= "ID",
+    CONCEPT_ID 		= CONCEPT 	+ ID,
+    CASEBASE_ID 	= CASEBASE 	+ ID,
+    AMAL_FUNCTION_ID 	= AMAL_FUNCTION + ID,
+    ATTR_ID 		= ATTR 		+ ID,
+    CASE_ID 		= CASE 		+ ID,
+    INSTANCE_ID 	= INSTANCE 	+ ID,
+    SIM_FUNCTION_ID	= SIM_FUNCTION 	+ ID;
+    
+    
+ // myCBR-rest API: IDs vocabulary
+    public static final String 
+    CASE_IDS 			= CONCEPT + "IDs";
+    
+    
+    // myCBR-rest API: type vocabulary specific names
+    public static final String 
+    TYPE 		= "Type",
+    AMAL_FUNCTION_TYPE 	= AMAL_FUNCTION + TYPE,
+    ATTR_TYPE 	       	= ATTR + TYPE;
 
     
     // Path variables
-    // Path pattern : /___s
+    
+    // Path pattern: /___ss
     public static final String 
-    PATH_CONCEPTS 	= "/concepts",
-    PATH_CASEBASES 	= "/casebases",
-    PATH_AML_FUNCTIONS 	= "/amalgamationFunctions",
-    PATH_INSTANCES 	= "/instances",
-    PATH_ATTRIBUTES 	= "/attributes",
-    PATH_VALUES         = "/values",
-    PATH_SIM_FUNCTIONS  = "/similarityfunctions";
+    PATH 		="/",
+    PATH_CONCEPTS 	= PATH + CONCEPTS,
+    PATH_CASEBASES 	= PATH + CASEBASES,
+    PATH_AMAL_FUNCTIONS = PATH + AMAL_FUNCTIONS,
+    PATH_CASES 		= PATH + CASES,
+    PATH_ATTRIBUTES 	= PATH + ATTRS,
+    PATH_VALUE_RANGE    = PATH + VALUE + "RANGE",
+    PATH_SIM_FUNCTIONS  = PATH + SIM_FUNCTIONS;
    
-    // Path pattern : /___s/{___ID}
+    // Path pattern: /___s/{___ID}  
     public static final String 
-    PATH_CONCEPT_ID	 = PATH_CONCEPTS       + "/{conceptID}",
-    PATH_CB_ID 	  	 = PATH_CASEBASES      + "/{casebaseID}",
-    PATH_AML_FUNCTION_ID = PATH_AML_FUNCTIONS  + "/{amalgamationFunctionID}",
-    PATH_INSTANCE_ID 	 = PATH_INSTANCES      + "/{instanceID}",   
-    PATH_ATTRIBUTE_ID 	 = PATH_ATTRIBUTES     + "/{attributeID}", 
-    PATH_SIM_FUNCTION_ID = PATH_SIM_FUNCTIONS  + "/{similarityFunctionID}";  
+    PATH_CONCEPT_ID	  = PATH_CONCEPTS       + "/{" + CONCEPT_ID 	 + "}",
+    PATH_CASEBASE_ID 	  = PATH_CASEBASES      + "/{" + CASEBASE_ID 	 + "}",
+    PATH_AMAL_FUNCTION_ID = PATH_AMAL_FUNCTIONS + "/{" + AMAL_FUNCTION_ID+ "}",
+    PATH_CASE_ID 	  = PATH_CASES          + "/{" + CASE_ID	 + "}", 
+    PATH_ATTR_ID 	  = PATH_ATTRIBUTES     + "/{" + ATTR_ID         + "}",
+    PATH_SIM_FUNCTION_ID  = PATH_SIM_FUNCTIONS  + "/{" + SIM_FUNCTION_ID + "}";
     
-    // Path pattern : /concepts/{conceptID}/___s/{___ID}  
+    
+    // Path pattern: /concepts/{conceptID}/___s 
     public static final String 
-    PATH_CONCEPT_CASEBASES 	 = PATH_CONCEPT_ID + PATH_CASEBASES,
-    PATH_CONCEPT_CB_ID           = PATH_CONCEPT_ID + PATH_CB_ID,
-    PATH_CONCEPT_AML_FUNCTIONS   = PATH_CONCEPT_ID + PATH_AML_FUNCTIONS,
-    PATH_CONCEPT_AML_FUNCTION_ID = PATH_CONCEPT_ID + PATH_AML_FUNCTION_ID,
-    PATH_CONCEPT_INSTANCES 	 = PATH_CONCEPT_ID + PATH_INSTANCES,
-    PATH_CONCEPT_INSTANCE_ID     = PATH_CONCEPT_ID + PATH_INSTANCE_ID,
-    PATH_CONCEPT_ATTRIBUTES 	 = PATH_CONCEPT_ID + PATH_ATTRIBUTES,
-    PATH_CONCEPT_ATTRIBUTE_ID    = PATH_CONCEPT_ID + PATH_ATTRIBUTE_ID,
-    PATH_CONCEPT_SIM_FUNCTIONS   = PATH_CONCEPT_ID + PATH_SIM_FUNCTIONS,
-    PATH_CONCEPT_SIM_FUNCTION_ID = PATH_CONCEPT_ID + PATH_SIM_FUNCTION_ID;
+    PATH_CONCEPT_CASEBASES 	= PATH_CONCEPT_ID + PATH_CASEBASES,
+    PATH_CONCEPT_AMAL_FUNCTIONS = PATH_CONCEPT_ID + PATH_AMAL_FUNCTIONS,    
+    PATH_CONCEPT_CASES 	 	= PATH_CONCEPT_ID + PATH_CASES,  
+    PATH_CONCEPT_ATTRS 	 	= PATH_CONCEPT_ID + PATH_ATTRIBUTES;
     
-    //Path pattern : /concepts/{conceptID}/casebases/{casebaseID}/___s/{___ID}
+    
+ // Path pattern: /concepts/{conceptID}/___s/{___ID}  
     public static final String 
-    PATH_CONCEPT_CB_AML_FUNCTIONS   = PATH_CONCEPT_CB_ID + PATH_AML_FUNCTIONS,
-    PATH_CONCEPT_CB_AML_FUNCTION_ID = PATH_CONCEPT_CB_ID + PATH_AML_FUNCTION_ID,
-    PATH_CONCEPT_CB_INSTANCES       = PATH_CONCEPT_CB_ID + PATH_INSTANCES,
-    PATH_CONCEPT_CB_INSTANCE_ID     = PATH_CONCEPT_CB_ID + PATH_INSTANCE_ID,
-    PATH_CONCEPT_CB_ATTRIBUTES      = PATH_CONCEPT_CB_ID + PATH_ATTRIBUTES,
-    PATH_CONCEPT_CB_ATTRIBUTE_ID    = PATH_CONCEPT_CB_ID + PATH_ATTRIBUTE_ID,
-    PATH_CONCEPT_CB_SIM_FUNCTIONS   = PATH_CONCEPT_CB_ID + PATH_SIM_FUNCTIONS,
-    PATH_CONCEPT_CB_SIM_FUNCTION_ID = PATH_CONCEPT_CB_ID + PATH_SIM_FUNCTION_ID;
+    PATH_CONCEPT_CASEBASE_ID 	  = PATH_CONCEPT_ID + PATH_CASEBASE_ID,
+    PATH_CONCEPT_AMAL_FUNCTION_ID = PATH_CONCEPT_ID + PATH_AMAL_FUNCTION_ID,
+    PATH_CONCEPT_CASE_ID     	  = PATH_CONCEPT_ID + PATH_CASE_ID,
+    PATH_CONCEPT_ATTR_ID	  = PATH_CONCEPT_ID + PATH_ATTR_ID;
+	    
     
-    
+    //Path pattern: /concepts/{conceptID}/casebases/{casebaseID}/___s
     public static final String 
-    COMPUTE_SELF_SIMLARITY_STR = "computeSelfSimlarity",
-    EPHEMERAL_STR = "ephemeral",
-    RETRIEVAL_STR = "retrievalByCaseIDs",
-    RETRIEVAL_WITH_CONTENT_STR = "retrievalByIDWithContent";
+    PATH_CONCEPT_CASEBASE_AMAL_FUNCTIONS   = PATH_CONCEPT_CASEBASE_ID + PATH_AMAL_FUNCTIONS,
+    PATH_CONCEPT_CASEBASE_CASES            = PATH_CONCEPT_CASEBASE_ID + PATH_CASES;
     
-    // Path pattern : /___
+    //Path pattern: /concepts/{conceptID}/casebases/{casebaseID}/___s/{___ID}
     public static final String 
-    PATH_SELF_SIMLARITY 	= "/" + COMPUTE_SELF_SIMLARITY_STR,
-    PATH_EPHEMERAL 		= "/" + EPHEMERAL_STR,
-    PATH_RETRIEVAL 		= "/" + RETRIEVAL_STR,
-    PATH_RETRIEVAL_WITH_CONTENT = "/" + RETRIEVAL_WITH_CONTENT_STR;
+    PATH_CONCEPT_CASEBASE_AMAL_FUNCTION_ID = PATH_CONCEPT_CASEBASE_ID + PATH_AMAL_FUNCTION_ID,
+    PATH_CONCEPT_CASEBASE_CASE_ID          = PATH_CONCEPT_CASEBASE_ID + PATH_CASE_ID;
     
-    public static final String PATH_CONCEPT_CB_SELF_SIMLARITY = PATH_CONCEPT_CB_ID + PATH_SELF_SIMLARITY;
+    //Path pattern: /concepts/{conceptID}/attributes/{attributeID}/___
+    public static final String PATH_CONCEPT_ATTR_VALUE_RANGE = PATH_CONCEPT_ATTR_ID + PATH_VALUE_RANGE;
 
-    public static final String PATH_DEFAULT_EPHEMERAL = PATH_CONCEPT_CB_AML_FUNCTION_ID + PATH_EPHEMERAL;
+    //Path pattern: /concepts/{conceptID}/attributes/{attributeID}/___s
+    public static final String PATH_CONCEPT_ATTR_SIM_FUNCTIONS = PATH_CONCEPT_ATTR_ID + PATH_SIM_FUNCTIONS;
+    
+    //Path pattern: /concepts/{conceptID}/attributes/{attributeID}/___s/{___ID}
+    public static final String PATH_CONCEPT_ATTR_SIM_FUNCTION_ID = PATH_CONCEPT_ATTR_ID + PATH_SIM_FUNCTION_ID;
+    
+    
+    public static final String 
+    COMPUTE_SELF_SIMLARITY = "computeSelfSimlarity",
+    EPHEMERAL = "ephemeral",
+    RETRIEVAL_BY_CASE_IDS = "retrievalByCaseIDs",
+    RETRIEVAL_BY_CASE_ID_WITH_CONTENT = "retrievalByCaseIDWithContent";
+    
+    // Path pattern: /___
+    public static final String 
+    PATH_SELF_SIMLARITY 		= PATH + COMPUTE_SELF_SIMLARITY,
+    PATH_EPHEMERAL 			= PATH + EPHEMERAL,
+    PATH_RETRIEVAL 			= PATH + RETRIEVAL_BY_CASE_IDS,
+    PATH_RETRIEVAL_BY_CASE_ID_WITH_CONTENT 	= PATH + RETRIEVAL_BY_CASE_ID_WITH_CONTENT;
+    
+    public static final String PATH_CONCEPT_CASEBASE_SELF_SIMLARITY = PATH_CONCEPT_CASEBASE_ID + PATH_SELF_SIMLARITY;
+
+    public static final String PATH_DEFAULT_EPHEMERAL = PATH_CONCEPT_CASEBASE_AMAL_FUNCTION_ID + PATH_EPHEMERAL;
     public static final String PATH_EPHEMERAL_RETRIEVAL = PATH_DEFAULT_EPHEMERAL + PATH_RETRIEVAL;
-    public static final String PATH_EPHEMERAL_RETRIEVAL_WITH_CONTENT = PATH_DEFAULT_EPHEMERAL + PATH_RETRIEVAL_WITH_CONTENT;
+    public static final String PATH_EPHEMERAL_RETRIEVAL_WITH_CONTENT = PATH_DEFAULT_EPHEMERAL + PATH_RETRIEVAL_BY_CASE_ID_WITH_CONTENT;
     public static final String PATH_EPHEMERAL_SELF_SIMILARITY = PATH_DEFAULT_EPHEMERAL + PATH_SELF_SIMLARITY;
-
 }

c4ee4af1deee8354724387d692d512f59e0c11a9
amar <amar.jaiswal@ntnu.no>
2019-12-05 16:05:40 +0100

    major: added caseID in the results; closed github issue id: #26

--- src/main/java/no/ntnu/mycbr/rest/Case.java
+++ src/main/java/no/ntnu/mycbr/rest/Case.java
@@ -19,8 +19,8 @@ public class Case {
     public Case(String caseID) {
 
         Project project = App.getProject();
-        Concept myConcept = project.getConceptByID(CBREngine.getConceptName());
-        Instance instance = myConcept.getInstance(caseID);
+        Concept concept = project.getConceptByID(CBREngine.getConceptName());
+        Instance instance = concept.getInstance(caseID);
         if(instance != null)
             casecontent = new LinkedHashMap<>(getSortedCaseContent(instance));
         else
@@ -30,21 +30,23 @@ public class Case {
     public Case(String caseID, String conceptID) {
 
         Project project = App.getProject();
-        Concept myConcept = project.getConceptByID(conceptID);
-        Instance aInstance = myConcept.getInstance(caseID);
-        casecontent = new LinkedHashMap<>(getSortedCaseContent(aInstance));
+        Concept concept = project.getConceptByID(conceptID);
+        Instance instance = concept.getInstance(caseID);
+        
+        casecontent.put(CASE_ID, instance.getName());
+        casecontent.putAll(getSortedCaseContent(instance));
     }
 
     // Used by full results
-    public Case(String concept, String caseID, double similarity) {
+    public Case(String conceptID, String caseID, double similarity) {
 
         Project project = App.getProject();
-        Concept myConcept = project.getConceptByID(concept);
-        Instance aInstance = myConcept.getInstance(caseID);
+        Concept concept = project.getConceptByID(conceptID);
+        Instance instance = concept.getInstance(caseID);
 
         casecontent.put(SIMILARITY, Double.toString(similarity));
-        casecontent.put(CASE_ID, aInstance.getName());
-        casecontent.putAll(getSortedCaseContent(aInstance));
+        casecontent.put(CASE_ID, instance.getName());
+        casecontent.putAll(getSortedCaseContent(instance));
     }
 
     public LinkedHashMap<String, String> getCase() {

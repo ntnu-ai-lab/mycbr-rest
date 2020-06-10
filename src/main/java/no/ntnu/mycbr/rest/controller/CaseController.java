5fb4f8dcf13bb6752f6d45d60b9f212852abb34c
Niphram <theniphram@gmail.com>
2020-06-10 14:26:37 +0200

    Fix missing save

--- src/main/java/no/ntnu/mycbr/rest/controller/CaseController.java
+++ src/main/java/no/ntnu/mycbr/rest/controller/CaseController.java
@@ -29,196 +29,179 @@ import static no.ntnu.mycbr.rest.common.ApiPathConstants.*;
 import static no.ntnu.mycbr.rest.common.ApiOperationConstants.*;
 
 @RestController
-public class CaseController
-{
-    private static final String CASES_BY_PATTERN = "/casesByPattern";
+public class CaseController {
+	private static final String CASES_BY_PATTERN = "/casesByPattern";
 
-    @Autowired
-    private CaseService instanceService;
+	@Autowired
+	private CaseService instanceService;
 
-    private final Log logger = LogFactory.getLog(getClass());
+	private final Log logger = LogFactory.getLog(getClass());
 
-    //Get one instance
-    @ApiOperation(value = GET_CASE_BY_CASE_ID, nickname = GET_CASE_BY_CASE_ID)
-    @RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASEBASE_CASE_ID, 
-    headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public Map<String, String> getInstance(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID,
-	    @PathVariable(value=CASE_ID) String caseID) {
+	// Get one instance
+	@ApiOperation(value = GET_CASE_BY_CASE_ID, nickname = GET_CASE_BY_CASE_ID)
+	@RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASEBASE_CASE_ID, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public Map<String, String> getInstance(@PathVariable(value = CONCEPT_ID) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String casebaseID, @PathVariable(value = CASE_ID) String caseID) {
 
-	Project p = App.getProject();
-	
-	if (!p.getCaseBases().containsKey(casebaseID))
-	    return null;
+		Project p = App.getProject();
 
-	Instance instance = p.getInstance(caseID);
-	
-	Case queriedCase = new Case(instance.getName(),conceptID); 
-	
-	return queriedCase.getCase();
-    }
+		if (!p.getCaseBases().containsKey(casebaseID))
+			return null;
 
-    //Delete one instance
-    @ApiOperation(value = DELETE_CASE_BY_CASE_ID, nickname = DELETE_CASE_BY_CASE_ID)
-    @RequestMapping(method = RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASE_ID, 
-    headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public boolean deleteInstance(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID,
-	    @PathVariable(value=CASE_ID) String caseID) {
-	Project p = App.getProject();
-	if(!p.getCaseBases().containsKey(casebaseID))
-	    return false;
-	ICaseBase cb = p.getCaseBases().get(casebaseID);
-	if(cb.containsCase(casebaseID)==null)
-	    return false;
-	p.getCaseBases().get(casebaseID).removeCase(caseID);
-	return true;
-    }
+		Instance instance = p.getInstance(caseID);
 
-    // Get all instances in case base of a concept
-    @ApiOperation(value = GET_ALL_CASES, nickname = GET_ALL_CASES)
-    @RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASES, headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public List<Map<String, String>> getAllInstances(
-	    @PathVariable(value=CONCEPT_ID) String conceptID) {
-	
-	Project p = App.getProject();
+		Case queriedCase = new Case(instance.getName(), conceptID);
 
-	/*Query query = new Query(conceptID);
-
-        System.out.println("p get all instances size: "+p.getAllInstances().size());
-        //System.out.println("is getallcases"+App.getProject().getSuperConcept().getAllInstances().size());
-        //System.out.println("is getallcases"+App.getProject().getSuperConcept().getAllInstances().size());
-        List<Instance> instances = new ArrayList<>();
-        for(ICaseBase iCaseBase : p.getCaseBases().values()){
-            logger.info("casebase has "+iCaseBase.getCases().size()+" cases ");
-            instances.addAll(iCaseBase.getCases());
-        }*/
-	Collection<Instance> instances = p.getAllInstances();
-	List<Map<String, String>> ret = new LinkedList<>();
-	for(Instance instance : instances){
-	    if(instance.getConcept().getName().contentEquals(conceptID))
-		ret.add(new Case(instance.getName(),conceptID).getCase());
-	}
-	return ret;
-    }
-
-    // Get all instances of a concept
-    @ApiOperation(value = GET_ALL_CASES_FROM_CASEBASE, nickname = GET_ALL_CASES_FROM_CASEBASE)
-    @RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASEBASE_CASES, 
-    headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public List<LinkedHashMap<String, String>> getAllInstancesInCaseBase(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID) {
-	
-	Query query = new Query(casebaseID,conceptID);
-	//TODO: filter to one type of concept
-	List<LinkedHashMap<String, String>> cases = getFullResult(query, conceptID);
-	return cases;
-    }
-
-
-    //Delete all instances
-    @ApiOperation(value=DELETE_ALL_CASES, nickname=DELETE_ALL_CASES)
-    @RequestMapping(method=RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASES)
-    @ApiResponsesDefault
-    public boolean deleteInstances(
-	    @PathVariable(value=CONCEPT) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID) {
-
-	Project p = App.getProject();
-	if(!p.getCaseBases().containsKey(casebaseID))
-	    return false;
-	Collection<Instance> collection = p.getCaseBases().get(casebaseID).getCases();
-	for(Instance i : collection){
-	    collection.remove(i);
+		return queriedCase.getCase();
 	}
 
-	return true;
-    }
-
-
-    //Delete instances according to pattern
-    @ApiOperation(value=DELETE_ALL_CASES_IN_CASEBASE_USING_PATTERN, nickname=DELETE_ALL_CASES_IN_CASEBASE_USING_PATTERN)
-    @RequestMapping(method=RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASES + CASES_BY_PATTERN)
-    @ApiResponsesDefault
-    public boolean deleteInstancePattern(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String caseBase,
-	    @RequestParam(value="pattern",defaultValue="*") String pattern) {
-
-	Project p = App.getProject();
-	if(!p.getCaseBases().containsKey(caseBase))
-	    return false;
-	if(pattern.contentEquals("*")){//this means delete all
-	    Collection<Instance> collection = p.getCaseBases().get(caseBase).getCases();
-	    Iterator<Instance> it = collection.iterator();
-	    while(it.hasNext()){
-		p.removeCase(it.next().getName());
-	    }
-	    p.save();
-	}else{
-	    Collection<Instance> collection = p.getCaseBases().get(caseBase).getCases();
-	    for(Instance i : collection){
-		collection.remove(i);
-	    }
-
+	// Delete one instance
+	@ApiOperation(value = DELETE_CASE_BY_CASE_ID, nickname = DELETE_CASE_BY_CASE_ID)
+	@RequestMapping(method = RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASE_ID, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public boolean deleteInstance(@PathVariable(value = CONCEPT_ID) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String casebaseID, @PathVariable(value = CASE_ID) String caseID) {
+		Project p = App.getProject();
+		if (!p.getCaseBases().containsKey(casebaseID))
+			return false;
+		ICaseBase cb = p.getCaseBases().get(casebaseID);
+		if (cb.containsCase(casebaseID) == null)
+			return false;
+		p.getCaseBases().get(casebaseID).removeCase(caseID);
+		return true;
 	}
-	return true;
-    }
 
-    /*
-    Add instances
-    input should be:
-    {cases:
-    [
-    {id:"caseid0",otherattribute:value,..}
-    {id:"caseid1",otherattribute:value,..}
-    ]
-    }
-     */
-    @ApiOperation(value = ADD_MULTIPLE_CASES_USING_JSON, nickname = ADD_MULTIPLE_CASES_USING_JSON)
-    @RequestMapping(method = RequestMethod.POST, value = PATH_CONCEPT_CASEBASE_CASES, 
-    headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public ArrayList<String> addInstancesJSON(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID,
-	    @RequestBody(required= true) JSONObject json) {
+	// Get all instances in case base of a concept
+	@ApiOperation(value = GET_ALL_CASES, nickname = GET_ALL_CASES)
+	@RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASES, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public List<Map<String, String>> getAllInstances(@PathVariable(value = CONCEPT_ID) String conceptID) {
 
-	Project p = App.getProject();
-	if(!p.getCaseBases().containsKey(casebaseID)){
-	    return new ArrayList<>();
+		Project p = App.getProject();
+
+		/*
+		 * Query query = new Query(conceptID);
+		 * 
+		 * System.out.println("p get all instances size: "+p.getAllInstances().size());
+		 * //System.out.println("is getallcases"+App.getProject().getSuperConcept().
+		 * getAllInstances().size());
+		 * //System.out.println("is getallcases"+App.getProject().getSuperConcept().
+		 * getAllInstances().size()); List<Instance> instances = new ArrayList<>();
+		 * for(ICaseBase iCaseBase : p.getCaseBases().values()){
+		 * logger.info("casebase has "+iCaseBase.getCases().size()+" cases ");
+		 * instances.addAll(iCaseBase.getCases()); }
+		 */
+		Collection<Instance> instances = p.getAllInstances();
+		List<Map<String, String>> ret = new LinkedList<>();
+		for (Instance instance : instances) {
+			if (instance.getConcept().getName().contentEquals(conceptID))
+				ret.add(new Case(instance.getName(), conceptID).getCase());
+		}
+		return ret;
 	}
-	Concept c = (Concept)p.getSubConcepts().get(conceptID);
 
-	JSONArray newCases = (JSONArray) json.get(CASES);
+	// Get all instances of a concept
+	@ApiOperation(value = GET_ALL_CASES_FROM_CASEBASE, nickname = GET_ALL_CASES_FROM_CASEBASE)
+	@RequestMapping(method = RequestMethod.GET, value = PATH_CONCEPT_CASEBASE_CASES, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public List<LinkedHashMap<String, String>> getAllInstancesInCaseBase(
+			@PathVariable(value = CONCEPT_ID) String conceptID, @PathVariable(value = CASEBASE_ID) String casebaseID) {
 
-	return instanceService.addInstances(c,casebaseID, newCases);
-    }
-
-    @ApiOperation(value = ADD_CASE_USING_JSON, nickname = ADD_CASE_USING_JSON)
-    @RequestMapping(method = RequestMethod.PUT, value = PATH_CONCEPT_CASEBASE_CASE_ID, 
-    headers=ACCEPT_APPLICATION_JSON)
-    @ApiResponsesDefault
-    public boolean addInstanceJSON(
-	    @PathVariable(value=CONCEPT_ID) String conceptID,
-	    @PathVariable(value=CASEBASE_ID) String casebaseID,
-	    @PathVariable(value=CASE_ID) String caseID,
-	    @RequestBody(required= true) JSONObject json) {
-	
-	Project p = App.getProject();
-	if(!p.getCaseBases().containsKey(casebaseID)){
-	    return false;
+		Query query = new Query(casebaseID, conceptID);
+		// TODO: filter to one type of concept
+		List<LinkedHashMap<String, String>> cases = getFullResult(query, conceptID);
+		return cases;
 	}
-	ICaseBase cb = p.getCaseBases().get(casebaseID);
-	Concept c = (Concept)p.getSubConcepts().get(conceptID);
-	
-	return null != instanceService.addInstance(c, cb, caseID, json);
-    }
+
+	// Delete all instances
+	@ApiOperation(value = DELETE_ALL_CASES, nickname = DELETE_ALL_CASES)
+	@RequestMapping(method = RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASES)
+	@ApiResponsesDefault
+	public boolean deleteInstances(@PathVariable(value = CONCEPT) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String casebaseID) {
+
+		Project p = App.getProject();
+		if (!p.getCaseBases().containsKey(casebaseID))
+			return false;
+		Collection<Instance> collection = p.getCaseBases().get(casebaseID).getCases();
+		for (Instance i : collection) {
+			collection.remove(i);
+		}
+
+		return true;
+	}
+
+	// Delete instances according to pattern
+	@ApiOperation(value = DELETE_ALL_CASES_IN_CASEBASE_USING_PATTERN, nickname = DELETE_ALL_CASES_IN_CASEBASE_USING_PATTERN)
+	@RequestMapping(method = RequestMethod.DELETE, value = PATH_CONCEPT_CASEBASE_CASES + CASES_BY_PATTERN)
+	@ApiResponsesDefault
+	public boolean deleteInstancePattern(@PathVariable(value = CONCEPT_ID) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String caseBase,
+			@RequestParam(value = "pattern", defaultValue = "*") String pattern) {
+
+		Project p = App.getProject();
+		if (!p.getCaseBases().containsKey(caseBase))
+			return false;
+		if (pattern.contentEquals("*")) {// this means delete all
+			Collection<Instance> collection = p.getCaseBases().get(caseBase).getCases();
+			Iterator<Instance> it = collection.iterator();
+			while (it.hasNext()) {
+				p.removeCase(it.next().getName());
+			}
+			p.save();
+		} else {
+			Collection<Instance> collection = p.getCaseBases().get(caseBase).getCases();
+			for (Instance i : collection) {
+				collection.remove(i);
+			}
+
+		}
+		return true;
+	}
+
+	/*
+	 * Add instances input should be: {cases: [
+	 * {id:"caseid0",otherattribute:value,..} {id:"caseid1",otherattribute:value,..}
+	 * ] }
+	 */
+	@ApiOperation(value = ADD_MULTIPLE_CASES_USING_JSON, nickname = ADD_MULTIPLE_CASES_USING_JSON)
+	@RequestMapping(method = RequestMethod.POST, value = PATH_CONCEPT_CASEBASE_CASES, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public ArrayList<String> addInstancesJSON(@PathVariable(value = CONCEPT_ID) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String casebaseID, @RequestBody(required = true) JSONObject json) {
+
+		Project p = App.getProject();
+		if (!p.getCaseBases().containsKey(casebaseID)) {
+			return new ArrayList<>();
+		}
+		Concept c = (Concept) p.getSubConcepts().get(conceptID);
+
+		JSONArray newCases = (JSONArray) json.get(CASES);
+
+		return instanceService.addInstances(c, casebaseID, newCases);
+	}
+
+	@ApiOperation(value = ADD_CASE_USING_JSON, nickname = ADD_CASE_USING_JSON)
+	@RequestMapping(method = RequestMethod.PUT, value = PATH_CONCEPT_CASEBASE_CASE_ID, headers = ACCEPT_APPLICATION_JSON)
+	@ApiResponsesDefault
+	public boolean addInstanceJSON(@PathVariable(value = CONCEPT_ID) String conceptID,
+			@PathVariable(value = CASEBASE_ID) String casebaseID, @PathVariable(value = CASE_ID) String caseID,
+			@RequestBody(required = true) JSONObject json) {
+
+		Project p = App.getProject();
+		if (!p.getCaseBases().containsKey(casebaseID)) {
+			return false;
+		}
+		ICaseBase cb = p.getCaseBases().get(casebaseID);
+		Concept c = (Concept) p.getSubConcepts().get(conceptID);
+
+		Instance success = instanceService.addInstance(c, cb, caseID, json);
+
+		if (success != null) {
+			p.save();
+		}
+
+		return success != null;
+	}
 }
